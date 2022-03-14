c61b323a09109ce55a0bb0b628d5c832923cbdb1
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-03-14 15:40:43 -0300

    test utils implemented

---
+++ test/unit/utils/testUtil.js
@@ -0,0 +1,47 @@
+import { Readable, Writable } from 'stream'
+import { jest } from '@jest/globals'
+
+const generateReadableStream = (data) => new Readable({
+  read() {
+    for (const item of data) {
+      this.push(item)
+    }
+
+    this.push(null)
+  }
+})
+
+const generateWritableStream = (onData) => new Writable({
+  write(chunk, enc, cb) {
+    onData(chunk)
+
+    cb(null, chunk)
+  }
+})
+
+const defaultHandleParams = () => {
+  const requestStream = generateReadableStream(['req body'])
+  const responseStream = generateWritableStream(() => { })
+  const data = {
+    request: {
+      ...requestStream,
+      headers: {},
+      method: '',
+      url: ''
+    },
+    response: {
+      ...responseStream,
+      writeHead: jest.fn(),
+      end: jest.fn()
+    }
+  }
+
+  return {
+    values: ()=> Object.values(data),
+    ...data
+  }
+}
+
+export default {
+  defaultHandleParams
+}
