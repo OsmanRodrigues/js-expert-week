14491d188bdbd876dac4b57c5fe20f267060f640
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-03-16 11:39:09 -0300

    client stream service implemented

--- server/service.js
+++ server/service.js
@@ -1,8 +1,29 @@
+import { randomUUID } from 'crypto'
 import fs, {promises as fsPromises} from 'fs'
-import { extname } from 'path';
-import { getPath } from './config.js';
+import { extname } from 'path'
+import { PassThrough } from 'stream'
+import { getPath } from './config.js'
 
 export class Service {
+
+  constructor() {
+    this.clientStreams = new Map() 
+  }
+
+  createClientStream() {
+    const id = randomUUID()
+    const clientStream = new PassThrough()
+    this.clientStreams.set(id, clientStream)
+
+    return {
+      id,
+      clientStream
+    }
+  }
+
+  removeClientStream(id) {
+    this.clientStreams.delete(id)
+  }
 
   createFileStream(filePath ) {
     return fs.createReadStream(filePath)
