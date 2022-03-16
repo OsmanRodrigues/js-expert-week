c1cf9a26b6e443f0927ab8e8b51ffa9b2726b0a1
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-03-16 11:39:32 -0300

    client stream controller implemented

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
-import { Service } from "./service.js"
+import { Service } from './service.js'
+import { logger } from './utils.js'
 
 export class Controller {
   
@@ -8,6 +9,23 @@ export class Controller {
 
   async getFileStream(fileName){
     return this.service.getFileStream(fileName)
+  }
+  
+  createClientStream() {
+    const { 
+      id,
+      clientStream
+    } = this.service.createClientStream()
+
+    const onClose = () => {
+      logger.info(`Closing connection stream of ${id}`)
+      this.service.removeClientStream(id)
+    }
+
+    return {
+      stream: clientStream,
+      onClose
+    }
   }
   
 }
