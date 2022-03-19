58a7610e16c9159b84d09be2c858a9e934f9ff5c
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-03-18 21:08:09 -0300

    heroku setup

--- server/index.js
+++ server/index.js
@@ -4,4 +4,9 @@ import { logger } from "./utils.js"
 
 server
   .listen(config.port)
-  .on('listening', ()=> logger.info(`Server running at port ${config.port}`))
+  .on('listening', () => logger.info(`Server running at port ${config.port}`))
+  
+process.on('uncaughtException', (err) =>
+  logger.error(`uncaughtException happened: ${err.stack || err}`))
+process.on('unhandledRejection', (err) =>
+  logger.error(`unhandledRejection happened: ${err.stack || err}`))
